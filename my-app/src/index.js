82e2b701218fdc4e64b1e8faab3d46af76efacfc
Uriel2404 <155695320+Uriel2404@users.noreply.github.com>
2024-07-19 22:31:28 -0600

    index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Login from './Login';
 import Register from './Register';
+import ForgotPassword from './ForgotPassword';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => (
-  <React.StrictMode>
-    <div className="container-fluid">
-      <div className="row">
-        <Login />
-        <Register />
+const App = () => {
+  const [currentPage, setCurrentPage] = React.useState('login');
+
+  const handleShowForgotPassword = () => {
+    setCurrentPage('forgotPassword');
+  };
+
+  const handleBackToLogin = () => {
+    setCurrentPage('login');
+  };
+
+  let currentPageComponent = <Login onForgotPassword={handleShowForgotPassword} />;
+  if (currentPage === 'forgotPassword') {
+    currentPageComponent = <ForgotPassword onBackToLogin={handleBackToLogin} />;
+  }
+
+  return (
+    <React.StrictMode>
+      <div className="container-fluid">
+        <div className="row">
+          {currentPageComponent}
+          <Register />
+        </div>
       </div>
-    </div>
-  </React.StrictMode>
-);
+    </React.StrictMode>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
